bffaf8bb13f877d7f1bc260e35dec04fdfa3b325
Neal Morton <mortonne@gmail.com>
2021-03-18 17:47:18 -0500

    build: compile cython modules as extensions

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 import setuptools
 import glob
+from Cython.Build import cythonize
 
 scripts = glob.glob('bin/*')
-setuptools.setup(scripts=scripts)
+setuptools.setup(scripts=scripts, ext_modules=cythonize('src/tesser/*.pyx'))
