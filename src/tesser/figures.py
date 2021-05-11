ab688811ab8bef5b5724938f2607c2645227c1c3
Neal Morton <mortonne@gmail.com>
2021-05-11 18:07:25 -0500

    refactor: remove plot_swarm_error
    Refactor to use mindstorm.statplot.plot_swarm_bar instead of tesser.figures.plot_swarm_error.

--- src/tesser/figures.py
+++ src/tesser/figures.py
@@ -6,49 +6,6 @@ from matplotlib import colors
 import seaborn as sns
 
 
-def plot_swarm_error(
-    data, x=None, y=None, hue=None, dark=None, light=None, ax=None, dodge=False,
-    capsize=.425
-):
-    """Make a bar plot with individual points and error bars."""
-    if dark is None:
-        dark = 'ch:rot=-.5, light=.7, dark=.3, gamma=.6'
-    if light is None:
-        light = 'ch:rot=-.5, light=.7, dark=.3, gamma=.2'
-
-    if ax is None:
-        ax = plt.gca()
-
-    # plot individual points
-    sns.swarmplot(
-        data=data.reset_index(), x=x, y=y, hue=hue, palette=dark, size=3,
-        linewidth=0.1, edgecolor='k', ax=ax, zorder=3, dodge=dodge
-    )
-
-    # plot error bars for the mean
-    sns.barplot(
-        data=data.reset_index(), x=x, y=y, hue=hue, ax=ax, dodge=dodge, color='k',
-        palette=light, errwidth=.8, capsize=capsize, edgecolor='k', linewidth=.75,
-        errcolor='k'
-    )
-
-    # remove overall xlabel and increase size of x-tick labels
-    ax.set_xlabel('')
-    ax.tick_params(axis='x', labelsize='large')
-
-    # fix ordering of plot elements
-    plt.setp(ax.lines, zorder=100, linewidth=1.25, label=None)
-    plt.setp(ax.collections, zorder=100, label=None)
-
-    # delete legend (redundant with the x-tick labels)
-    legend = ax.get_legend()
-    if legend is not None:
-        legend.remove()
-        if hue is not None:
-            # refresh the legend to remove the swarm points
-            ax.legend()
-
-
 def plot_group_mat(
     data, x='dim1', y='dim2', hue=None, palette=None, color=None, ax=None
 ):
