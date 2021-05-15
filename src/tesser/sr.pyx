82d28d049c6bf45aad989954251363ac8cd47528
Neal Morton <mortonne@gmail.com>
2021-05-15 12:51:03 -0500

    feat: cython function for choice probability

--- src/tesser/sr.pyx
+++ src/tesser/sr.pyx
@@ -1,4 +1,5 @@
 cimport cython
+from libc.math cimport exp
 
 @cython.boundscheck(False)
 @cython.wraparound(False) 
@@ -31,3 +32,29 @@ def learn_sr(
             M_new = hot + gamma * M[s_new, i]
             M[s, i] = (1 - alpha) * M[s, i] + alpha * M_new
         s = s_new
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.cdivision(True)
+cpdef prob_choice_sim1(
+    int cue,
+    int opt1,
+    int opt2,
+    int response,
+    double [:,:] sim,
+    double tau,
+):
+    """Choice probability given one similarity matrix."""
+    cdef double support1
+    cdef double support2
+    cdef double support
+    cdef double prob
+    support1 = sim[cue, opt1]
+    support2 = sim[cue, opt2]
+    if response == 0:
+        support = support1
+    else:
+        support = support2
+    prob = exp(support / tau) / (exp(support1 / tau) + exp(support2 / tau))
+    return prob
