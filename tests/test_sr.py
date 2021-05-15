183e92cec24015b2c5f71cd6ec6fe4aaaa0fa720
Neal Morton <mortonne@gmail.com>
2021-05-15 13:14:32 -0500

    test: choice probability with two similarity matrices

--- tests/test_sr.py
+++ tests/test_sr.py
@@ -39,3 +39,26 @@ def test_choice_prob_sim1():
     tau = 0.5
     prob = sr.prob_choice_sim(cue, opt1, opt2, response, sim, tau)
     np.testing.assert_allclose(prob, 0.11920292)
+
+
+def test_choice_prob_sim2():
+    """Test choice probability based on two similarity matrices."""
+    sim1 = np.array([[1, 2, 3], [3, 2, 1], [1, 3, 2]], dtype='double')
+    sim2 = np.array([[3, 2, 1], [1, 3, 2], [2, 1, 3]], dtype='double')
+    cue = 0
+    opt1 = 1
+    opt2 = 2
+    response = 0
+
+    tau = 1
+    w = 0.5
+    prob = sr.prob_choice_sim2(cue, opt1, opt2, response, sim1, sim2, w, tau)
+    np.testing.assert_allclose(prob, 0.5)
+
+    w = 1
+    prob = sr.prob_choice_sim2(cue, opt1, opt2, response, sim1, sim2, w, tau)
+    np.testing.assert_allclose(prob, 0.26894142)
+
+    w = 0
+    prob = sr.prob_choice_sim2(cue, opt1, opt2, response, sim1, sim2, w, tau)
+    np.testing.assert_allclose(prob, 0.731058578)
