f91e98bd7c83d239b9e482ea8a52f9a72d16b15e
Neal Morton <mortonne@gmail.com>
2021-05-15 18:57:13 -0500

    tests: evaluating with a custom similarity matrix

--- tests/test_sr.py
+++ tests/test_sr.py
@@ -230,3 +230,18 @@ def test_prob_struct_induct_question(struct_pandas, induct_pandas):
         [0.67672246, 0.32327754, 0.38313802, 0.61686198, 0.4378235, 0.5621765]
     )
     np.testing.assert_allclose(prob, expected)
+
+
+def test_prob_struct_induct_sim2(struct_pandas, induct_pandas):
+    """Test induction test probability with a custom matrix."""
+    param = {'alpha1': 0.5, 'gamma1': 0.9, 'tau': 1, 'w': 1}
+    sim1_spec = {'alpha': 'alpha1', 'gamma': 'gamma1'}
+    sim = np.ones((6, 6))
+    sim2_spec = {'sim': sim}
+    prob = model.prob_struct_induct(
+        struct_pandas, induct_pandas, param, sim1_spec, sim2_spec
+    )
+    expected = np.array(
+        [0.67672246, 0.32327754, 0.38313802, 0.61686198, 0.4378235, 0.5621765]
+    )
+    np.testing.assert_allclose(prob, expected)
