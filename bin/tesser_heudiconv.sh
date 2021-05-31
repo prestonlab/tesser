3ef23490aad77a6b19a5a7bcebf5d3eb18b4467b
Neal Morton <mortonne@gmail.com>
2021-05-31 15:47:00 -0500

    feat: script to run heudiconv

---
+++ bin/tesser_heudiconv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Run heudiconv to convert raw data for one subject.
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: tesser_heudiconv.sh subject raw_dir heuristic bids_dir"
+    exit 1
+fi
+
+subject=$1
+raw_dir=$2
+heuristic=$3
+bids_dir=$4
+
+subj_raw_dir=$raw_dir/$subject/raw/$subject
+if [[ ! -d $subj_raw_dir ]]; then
+    echo "Error: raw directory not found: $subj_raw_dir"
+    exit 1
+fi
+
+if [[ ! -f $heuristic ]]; then
+    echo "Error: heuristic file does not exist: $heuristic"
+    exit 1
+fi
+
+image=$STOCKYARD2/software/images/heudiconv-0.9.0.sif
+module load tacc-singularity
+singularity run "$image" \
+    -s "$subject" \
+    -f "$heuristic" \
+    -b \
+    -o "$bids_dir" \
+    --minmeta \
+    --files "$subj_raw_dir"/*/*.dcm
