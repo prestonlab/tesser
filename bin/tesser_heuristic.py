b9d5412c52e7f2cdb9ec1cf8e8cc7f4816a4dfb1
Neal Morton <mortonne@gmail.com>
2021-06-03 00:43:15 -0500

    docs: comments explaining heuristic file

--- bin/tesser_heuristic.py
+++ bin/tesser_heuristic.py
@@ -17,24 +17,7 @@ def infotodict(seqinfo):
     seqitem: run number during scanning
     subindex: sub index within group
     """
-
-    key_T1w = create_key('sub-{subject}/anat/sub-{subject}_T1w')
-    key_T2w = create_key('sub-{subject}/anat/sub-{subject}_run-{item}_T2w')
-    key_magnitude = create_key('sub-{subject}/fmap/sub-{subject}_run-{item}_magnitude')
-    key_phasediff = create_key('sub-{subject}/fmap/sub-{subject}_run-{item}_phasediff')
-    key_struct = create_key('sub-{subject}/func/sub-{subject}_task-struct_run-{item}_bold')
-    key_sbref = create_key('sub-{subject}/func/sub-{subject}_task-struct_run-{item}_sbref')
-
-    info = {
-        key_T1w: [],
-        key_T2w: [],
-        key_magnitude: [],
-        key_phasediff: [],
-        key_sbref: [],
-        key_struct: [],
-    }
-
-    # get just the functional runs that were completed
+    # find the functional runs that were completed
     include_sbref = []
     include_bold = []
     for i, s in enumerate(seqinfo):
@@ -45,6 +28,23 @@ def infotodict(seqinfo):
                 include_sbref.append(seqinfo[i - 1].series_id)
                 include_bold.append(s.series_id)
 
+    # define file path format for BIDS
+    key_T1w = create_key('sub-{subject}/anat/sub-{subject}_T1w')
+    key_T2w = create_key('sub-{subject}/anat/sub-{subject}_run-{item}_T2w')
+    key_magnitude = create_key('sub-{subject}/fmap/sub-{subject}_run-{item}_magnitude')
+    key_phasediff = create_key('sub-{subject}/fmap/sub-{subject}_run-{item}_phasediff')
+    key_struct = create_key('sub-{subject}/func/sub-{subject}_task-struct_run-{item}_bold')
+    key_sbref = create_key('sub-{subject}/func/sub-{subject}_task-struct_run-{item}_sbref')
+
+    # sort scans into file types
+    info = {
+        key_T1w: [],
+        key_T2w: [],
+        key_magnitude: [],
+        key_phasediff: [],
+        key_sbref: [],
+        key_struct: [],
+    }
     n_fieldmap = 0
     n_T2 = 0
     for s in seqinfo:
@@ -73,35 +73,47 @@ def infotodict(seqinfo):
         * image_type
         """
         if s.series_description == 'mprage':
+            # T1 highres anatomical
             info[key_T1w].append(s.series_id)
+
         elif 'T2' in s.series_description:
+            # T2 coronal anatomical
             n_T2 += 1
+
+            # for participants with extra scans, exclude the worst ones
             if s.patient_id == 'tesser_101' and n_T2 == 1:
-                # first scan was bad
                 continue
             elif s.patient_id == 'TESSER_108' and n_T2 == 3:
-                # first two scans were best
                 continue
+
+            # add scan if included
             info[key_T2w].append(s.series_id)
+
         elif s.series_description == 'fieldmap':
+            # fieldmaps to estimate susceptibility distortion
             n_fieldmap += 1
+
+            # if extra fieldmaps, exclude the earlier ones
             if s.patient_id == 'tesser_105' and n_fieldmap in [1, 2]:
-                # discard first fieldmap
                 continue
             elif s.patient_id == 'TESSER_109' and n_fieldmap in [3, 4]:
-                # discard second fieldmap
                 continue
             elif s.patient_id == 'tesser_122' and n_fieldmap in [1, 2]:
-                # discard first fieldmap
                 continue
+
+            # add to either magnitude or phase difference image set
             if n_fieldmap % 2 == 1:
                 info[key_magnitude].append(s.series_id)
             else:
                 info[key_phasediff].append(s.series_id)
+
         elif s.series_description == 'functional_run_SBRef':
+            # single-band image from a functional scan
             if s.series_id in include_sbref:
                 info[key_sbref].append(s.series_id)
+
         elif s.series_description == 'functional_run':
+            # functional scan
             if s.series_id in include_bold:
                 info[key_struct].append(s.series_id)
     return info
