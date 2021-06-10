d71ffc4eeca47609c5ef34aed6eb0c6e3cffc77e
Neal Morton <mortonne@gmail.com>
2021-06-10 15:54:36 -0500

    refactor: copy json file from resources

--- bin/tesser_bids_events.py
+++ bin/tesser_bids_events.py
@@ -4,7 +4,8 @@
 
 import os
 import argparse
-import json
+import shutil
+from pkg_resources import resource_filename
 
 from tesser import tasks
 
@@ -35,70 +36,8 @@ def main(study_dir, bids_dir):
         file = os.path.join(subj_dir, f'sub-{subject}_task-struct_run-{run}_events')
         data.to_csv(file + '.tsv', sep='\t', index=False, na_rep='n/a')
 
-    meta = {
-        "onset": {
-            "LongName": "Stimulus onset",
-            "Description": "Time the object appeared on screen",
-            "Units": "s",
-        },
-        "duration": {
-            "LongName": "Stimulus duration",
-            "Description": "Time the object remained on screen",
-            "Units": "s",
-        },
-        "trial_type": {
-            "LongName": "Block type",
-            "Description": "Type of object sequence block",
-            "Levels": {
-                "structured": "A random walk drawn from the temporal community graph",
-                "scrambled": "All objects in random order",
-            }
-        },
-        "block": {
-            "LongName": "Block number",
-            "Description": "Index of the current block",
-        },
-        "community": {
-            "LongName": "Community number",
-            "Description": "The community of the current object",
-        },
-        "object": {
-            "LongName": "Object number",
-            "Description": "The node of the current object",
-        },
-        "object_type": {
-            "LongName": "Object type",
-            "Description": "Node type of the object in the graph",
-            "Levels": {
-                "central": "A central node within a community",
-                "boundary": "A node connecting to another community",
-            }
-        },
-        "orientation": {
-            "LongName": "Object orientation",
-            "Description": "Presented orientation of the object",
-            "Levels": {
-                "canonical": "Same orientation as learned earlier",
-                "rotated": "Rotated 90 degrees from original",
-            }
-        },
-        "response": {
-            "LongName": "Response",
-            "Description": "Response to the rotation detection task",
-            "Levels": {
-                "canonical": "Same orientation as learned earlier",
-                "rotated": "Rotated 90 degrees from original",
-            }
-        },
-        "response_time": {
-            "LongName": "Response time",
-            "Description": "Time of response to rotation decision",
-            "Units": "s",
-        },
-    }
-    file = os.path.join(bids_dir, 'task-struct.json')
-    with open(file, 'w') as f:
-        json.dump(meta, f, indent=4)
+    json_file = resource_filename('tesser', 'data/task-struct_events.json')
+    shutil.copy(json_file, os.path.join(bids_dir, 'task-struct.json'))
 
 
 if __name__ == '__main__':
