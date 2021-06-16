4bdfe0891f98e5b35e1904d1afc9877c171c74de
Neal Morton <mortonne@gmail.com>
2021-06-16 14:34:10 -0500

    refactor: take main template dir and require space input

--- bin/tesser_template_rois.sh
+++ bin/tesser_template_rois.sh
@@ -10,19 +10,20 @@ fi
 templatedir=$1
 outdir=$2
 subject=$3
+space=$4
 
-reference_name=task-struct_run-1_space-MNI152NLin2009cAsym_desc-brain_mask
-reference_file=${outdir}/sub-${subject}/func/sub-${subject}_${reference_name}.nii.gz
+subjdir=${outdir}/sub-${subject}
+reference_name=task-struct_run-1_space-${space}_desc-brain_mask
+reference_file=${subjdir}/func/sub-${subject}_${reference_name}.nii.gz
 
 # brain mask
 name=desc-brain_mask
-template_file=${templatedir}/tpl-MNI152NLin2009cAsym_res-01_${name}.nii.gz
+template_file=${templatedir}/tpl-${space}/tpl-${space}_res-01_${name}.nii.gz
 if [[ ! -f ${template_file} ]]; then
     echo "Template file not found: ${template_file}"
     exit 1
 fi
-out_name=space-MNI152NLin2009cAsym_${name}
-output_file=${outdir}/sub-${subject}/anat/sub-${subject}_${out_name}.nii.gz
+output_file=${subjdir}/anat/sub-${subject}_space-${space}_${name}.nii.gz
 antsApplyTransforms -d 3 -e 0 \
     -i "${template_file}" \
     -r "${reference_file}" \
@@ -31,13 +32,12 @@ antsApplyTransforms -d 3 -e 0 \
 
 # probabilistic labels
 for name in label-{brain,GM,WM,CSF}_probseg; do
-    template_file=${templatedir}/tpl-MNI152NLin2009cAsym_res-01_${name}.nii.gz
+    template_file=${templatedir}/tpl-${space}/tpl-${space}_res-01_${name}.nii.gz
     if [[ ! -f ${template_file} ]]; then
         echo "Template file not found: ${template_file}"
         exit 1
     fi
-    out_name=space-MNI152NLin2009cAsym_${name}
-    output_file=${outdir}/sub-${subject}/anat/sub-${subject}_${out_name}.nii.gz
+    output_file=${subjdir}/anat/sub-${subject}_space-${space}_${name}.nii.gz
     antsApplyTransforms -d 3 -e 0 \
         -i "${template_file}" \
         -r "${reference_file}" \
