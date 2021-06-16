52403ec1c160a5f9b7fdf37ebd030a6344aedbe1
Neal Morton <mortonne@gmail.com>
2021-06-16 12:22:59 -0500

    refactor: take space as a required input

--- bin/tesser_smooth.sh
+++ bin/tesser_smooth.sh
@@ -2,14 +2,15 @@
 #
 # Smooth functional data and copy related metadata to the results directory.
 
-if [[ $# -lt 3 ]]; then
-    echo "Usage: tesser_smooth.sh prepdir outdir subject"
+if [[ $# -lt 4 ]]; then
+    echo "Usage: tesser_smooth.sh prepdir outdir subject space"
     exit 1
 fi
 
 prepdir=$1
 outdir=$2
 subject=$3
+space=$4
 
 prepsubj=$prepdir/sub-$subject
 outsubj=$outdir/sub-$subject
@@ -25,15 +26,17 @@ cp "$prepsubj/anat/sub-${subject}_from-T1w_to-MNI152NLin2009cAsym_mode-image_xfm
 for run in 1 2 3 4 5 6; do
     echo "Smoothing run ${run}..."
     base="sub-${subject}_task-struct_run-${run}"
-    cp "$prepsubj/func/${base}_space-T1w_desc-brain_mask".{nii.gz,json} "$outsubj/func"
-    cp "$prepsubj/func/${base}_space-T1w_boldref.nii.gz" "$outsubj/func"
     cp "$prepsubj/func/${base}_desc-confounds_timeseries".{tsv,json} "$outsubj/func"
-    cp "$prepsubj/func/${base}_desc-preproc_bold.json" "$outsubj/func"
     cp "$prepsubj/func/${base}_from-scanner_to-T1w_mode-image_xfm.txt" "$outsubj/func"
     cp "$prepsubj/func/${base}_from-T1w_to-scanner_mode-image_xfm.txt" "$outsubj/func"
+
+    cp "$prepsubj/func/${base}_space-${space}_desc-brain_mask".{nii.gz,json} "$outsubj/func"
+    cp "$prepsubj/func/${base}_space-${space}_boldref.nii.gz" "$outsubj/func"
+    cp "$prepsubj/func/${base}_space-${space}_desc-preproc_bold.json" "$outsubj/func"
+
     smooth_susan \
-        "$prepsubj/func/${base}_space-T1w_desc-preproc_bold.nii.gz" \
-        "$prepsubj/func/${base}_space-T1w_desc-brain_mask.nii.gz" \
+        "$prepsubj/func/${base}_space-${space}_desc-preproc_bold.nii.gz" \
+        "$prepsubj/func/${base}_space-${space}_desc-brain_mask.nii.gz" \
         4.0 \
-        "$outsubj/func/${base}_space-T1w_desc-smooth4mm_bold.nii.gz"
+        "$outsubj/func/${base}_space-${space}_desc-smooth4mm_bold.nii.gz"
 done
