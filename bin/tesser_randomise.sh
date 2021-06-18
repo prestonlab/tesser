f0e57b61341e8d7a2544987a6c7387e73a9b170a
Neal Morton <mortonne@gmail.com>
2021-06-17 21:40:23 -0500

    refactor: save concatenated files to zstat_all

--- bin/tesser_randomise.sh
+++ bin/tesser_randomise.sh
@@ -13,9 +13,9 @@ resname=$3
 n_perm=$4
 
 mkdir -p "${resdir}/${resname}"
-fslmerge -t "${resdir}/${resname}/zstat.nii.gz" \
+fslmerge -t "${resdir}/${resname}/zstat_all.nii.gz" \
     "${resdir}"/sub-*/sub-*_desc-"${contrast}"_zstat.nii.gz
 
-randomise -i "${resdir}/${resname}/zstat.nii.gz" \
+randomise -i "${resdir}/${resname}/zstat_all.nii.gz" \
     -o "${resdir}/${resname}/zstat" \
     -1 -n "$n_perm" -x --uncorrp
