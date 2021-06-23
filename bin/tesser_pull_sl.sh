6c2373fe31e721886e063251d5f37c7147f798a5
Neal Morton <mortonne@gmail.com>
2021-06-23 03:23:50 -0500

    feat: script to pull searchlight results

---
+++ bin/tesser_pull_sl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Pull searchlight results.
+
+if [[ $# -lt 2 ]]; then
+    echo "Usage: pull_tesser_sl.sh src dest [rsync flags]"
+    exit 1
+fi
+
+src=$1
+dest=$2
+shift 2
+
+rsync -azvu "$src" "$dest" \
+    --include=beta/ \
+    --include="beta/smooth?mm/" \
+    --include="beta/smooth?mm/*/" \
+    --include="beta/smooth?mm/*/community/" \
+    --include="beta/smooth?mm/*/community_bias/" \
+    --include="*.txt" \
+    --include="*.nii.gz" \
+    --exclude="*" \
+    --prune-empty-dirs \
+    "$@"
